7d8dca4920f9f01d015a05b4976dc206f64fb74e
Cameron Lares <cameronlares@gmail.com>
2023-02-04 14:31:27 -0800

    Added ability to Press either an x or an o

--- App.js
+++ App.js
@@ -1,17 +1,65 @@
+import React, { useState} from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Pressable,Alert } from 'react-native'
 import bg from './assets/bg.jpeg'
 export default function App() {
+  const [map, setMap] = useState([
+    ['o', '', ''], // 1st row
+    ['', '', 'x'], // 2nd row
+    ['', '', 'x'] // 3rd row
+  ])
+const onPress = ( rowIndex,columnIndex ) => {
+console.warn("Row", rowIndex,"Column", columnIndex );
+
+if(map[rowIndex][columnIndex] !=""){
+  Alert.alert("Position already occupied");
+  return;
+}
+
+//Expects a new object or a function be called with an existing state,
+// returns updated info
+
+// useEffect ( () => {
+//   setMap((existingMap) => { 
+//     existingMap[rowIndex][columnIndex] = "x";
+//   return existingMap;
+//   });
+// },[map]) 
+
+setMap((existingMap) => { 
+  const updatedMap = [...existingMap]
+  updatedMap[rowIndex][columnIndex] = "x";
+return updatedMap;
+});
+
+};
   return (
     <View style={styles.container}>
-      <ImageBackground source={bg} style={styles.bg} resizeMode='contain'>
-        <View style={styles.circle} />
-        <View>
-          <View style={styles.crossLine} />
-          {/* //Add Two Different Styles To A Component */}
-          <View style={[styles.crossLine, styles.crossLineReversed]} />
-          <View />
+      <ImageBackground source={bg} style={styles.bg} resizeMode='cover'>
+        <View style={styles.map}>
+          {map.map( (row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {row.map((cell, columnIndex) => (
+                <Pressable key={columnIndex} onPress={()=> onPress(rowIndex,columnIndex)} style={styles.cell}>
+                  {cell == 'o' && <View style={styles.circle} />}
+                  {cell == 'x' &&
+                    <View style={styles.cross}>
+                      <View style={styles.crossLine} />
+                      <View style={[styles.crossLine, styles.crossLineReversed]} />
+                    </View>
+                  }
+                </Pressable>
+              ))}
+            </View>
+          ))}
 
+          {/* <View style={styles.circle} />
+          <View style={styles.cross}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineReversed]} />
+          </View> 
+          
+          */}
         </View>
       </ImageBackground>
       <StatusBar style='auto' />
@@ -34,43 +82,59 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 20
   },
-  circle: {
-    width: 75,
-    height: 75,
+  map: {
+    borderWidth: 1,
+    borderColor: 'white',
+    width: '80%',
+    aspectRatio: 1
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+    // borderColor: 'red',
+    // borderWidth: 2,
+  },
+  cell: {
+    width: 100,
+    height: 100,
+    flex: 1,
 
+    borderColor: 'white',
+    borderWidth: 1
+  },
+  circle: {
+    flex: 1,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
     margin: 10,
 
     borderWidth: 10,
-    borderColor: "white",
+    borderColor: 'white'
   },
-  innerCircle: {
-    width: 50,
-    height: 50,
-    backgroundColor: '#242D34',
-    borderRadius: 50,
+  cross: {
+ flex:1,
   },
   crossLine: {
-    position: 'absolute',
+    // position: 'relative',
+    left: '48%',
     width: 10,
-    height: 50,
+    height: '100%',
     backgroundColor: 'white',
     // Used Border Radius no BG color to create donut circle
-
+    position: 'absolute',
     borderRadius: 5,
     transform: [
       {
-        rotate: '45deg',
-      },
-    ],
+        rotate: '45deg'
+      }
+    ]
   },
   crossLineReversed: {
     transform: [
       {
-        rotate: '-45deg',
-      },
-    ],
+        rotate: '-45deg'
+      }
+    ]
   }
 })
